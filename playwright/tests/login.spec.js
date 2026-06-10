aea8567ef73093a16b10918c74432206deaedc61
Orransy <imanxy09@gmail.com>
2026-06-11 05:41:43 +0700

    Add files via upload

---
+++ playwright/tests/login.spec.js
@@ -0,0 +1,179 @@
+require('dotenv').config();
+const { test, expect } = require('@playwright/test');
+
+const VALID_USER = process.env.TEST_USERNAME;
+const VALID_PASS = process.env.TEST_PASSWORD;
+
+async function gotoHome(page) {
+    await page.goto(
+        'https://thuvienso.actvn.edu.vn/',
+        {
+            waitUntil: 'domcontentloaded',
+            timeout: 120000
+        }
+    );
+
+    await page.waitForSelector('#txtLoginUsername');
+}
+
+async function clickLogin(page) {
+    await page.locator('button[onclick="login();"]').click();
+    await page.waitForTimeout(3000);
+}
+
+test.describe('ACTVN LOGIN TEST', () => {
+
+    test.beforeEach(async ({ page }) => {
+        await gotoHome(page);
+    });
+
+    // TC01
+    test('Login thành công', async ({ page }) => {
+
+        await page.fill('#txtLoginUsername', VALID_USER);
+        await page.fill('#txtLoginPassword', VALID_PASS);
+
+        await clickLogin(page);
+
+        await expect(page.url()).toContain('/account/welcome');
+    });
+
+    // TC02
+    test('Password sai', async ({ page }) => {
+
+        await page.fill('#txtLoginUsername', VALID_USER);
+        await page.fill('#txtLoginPassword', 'wrongpassword');
+
+        await clickLogin(page);
+
+        await expect(page.url()).not.toContain('/account/welcome');
+    });
+
+    // TC03
+    test('Username sai', async ({ page }) => {
+
+        await page.fill('#txtLoginUsername', 'wronguser');
+        await page.fill('#txtLoginPassword', VALID_PASS);
+
+        await clickLogin(page);
+
+        await expect(page.url()).not.toContain('/account/welcome');
+    });
+
+    // TC04
+    test('Username và Password sai', async ({ page }) => {
+
+        await page.fill('#txtLoginUsername', 'abc');
+        await page.fill('#txtLoginPassword', 'xyz');
+
+        await clickLogin(page);
+
+        await expect(page.url()).not.toContain('/account/welcome');
+    });
+
+    // TC05
+    test('Username rỗng', async ({ page }) => {
+
+        await page.fill('#txtLoginPassword', VALID_PASS);
+
+        await clickLogin(page);
+
+        await expect(page.url()).not.toContain('/account/welcome');
+    });
+
+    // TC06
+    test('Password rỗng', async ({ page }) => {
+
+        await page.fill('#txtLoginUsername', VALID_USER);
+
+        await clickLogin(page);
+
+        await expect(page.url()).not.toContain('/account/welcome');
+    });
+
+    // TC07
+    test('Username và Password rỗng', async ({ page }) => {
+
+        await clickLogin(page);
+
+        await expect(page.url()).not.toContain('/account/welcome');
+    });
+
+    // TC08
+    test('Username có khoảng trắng đầu', async ({ page }) => {
+
+        await page.fill('#txtLoginUsername', ` ${VALID_USER}`);
+        await page.fill('#txtLoginPassword', VALID_PASS);
+
+        await clickLogin(page);
+
+        await expect(page.url()).not.toContain('/account/welcome');
+    });
+
+    // TC09
+    test('Username có khoảng trắng cuối', async ({ page }) => {
+
+        await page.fill('#txtLoginUsername', `${VALID_USER} `);
+        await page.fill('#txtLoginPassword', VALID_PASS);
+
+        await clickLogin(page);
+
+        await expect(page.url()).not.toContain('/account/welcome');
+    });
+
+    // TC11
+    test('Password có khoảng trắng đầu', async ({ page }) => {
+
+        await page.fill('#txtLoginUsername', VALID_USER);
+        await page.fill('#txtLoginPassword', ` ${VALID_PASS}`);
+
+        await clickLogin(page);
+
+        await expect(page.url()).not.toContain('/account/welcome');
+    });
+
+    // TC12
+    test('Password có khoảng trắng cuối', async ({ page }) => {
+
+        await page.fill('#txtLoginUsername', VALID_USER);
+        await page.fill('#txtLoginPassword', `${VALID_PASS} `);
+
+        await clickLogin(page);
+
+        await expect(page.url()).not.toContain('/account/welcome');
+    });
+
+    // TC10
+    test('Username rất dài', async ({ page }) => {
+
+        await page.fill('#txtLoginUsername', 'a'.repeat(300));
+        await page.fill('#txtLoginPassword', VALID_PASS);
+
+        await clickLogin(page);
+
+        await expect(page.url()).not.toContain('/account/welcome');
+    });
+
+    // TC13
+    test('Password rất dài', async ({ page }) => {
+
+        await page.fill('#txtLoginUsername', VALID_USER);
+        await page.fill('#txtLoginPassword', 'a'.repeat(300));
+
+        await clickLogin(page);
+
+        await expect(page.url()).not.toContain('/account/welcome');
+    });
+
+    // TC14
+    test('SQL Injection', async ({ page }) => {
+
+        await page.fill('#txtLoginUsername', "' OR 1=1 --");
+        await page.fill('#txtLoginPassword', "' OR 1=1 --");
+
+        await clickLogin(page);
+
+        await expect(page.url()).not.toContain('/account/welcome');
+    });
+
+});
